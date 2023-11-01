35e6ae14cc7ce46c0551ec9232980709a0d3b6a0
ifpb <edemberg@ifpb.edu.br>
2023-11-01 18:08:48 -0300

    com devise

--- Gemfile
+++ Gemfile
@@ -76,3 +76,5 @@ gem "faker", "~> 3.2"
 gem "kaminari", "~> 1.2"
 
 gem "cssbundling-rails", "~> 1.3"
+
+gem "devise", "~> 4.9"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
   resources :people
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
